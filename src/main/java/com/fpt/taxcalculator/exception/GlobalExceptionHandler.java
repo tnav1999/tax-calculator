9bb23f6d67e52b754b58985d0b7387edf3ef3498
NCLAN\tnav <tnav@netcompany.com>
2025-03-13 09:28:25 +0700

    Added annotation for exception handler

--- src/main/java/com/fpt/taxcalculator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fpt/taxcalculator/exception/GlobalExceptionHandler.java
@@ -6,10 +6,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
